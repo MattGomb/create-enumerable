aa500112326b0f35f3062d6559cadf6a5eaa2da0
Matyas Gombos <gombosmm@gmail.com>
2022-12-22 13:51:57 -0500

    Add class and fix linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,17 +1,17 @@
 module MyEnumerable
-    def any?
-      each { |a| return true if yield a }
-      false
-    end
-
-    def all?
-      each { |a| return false unless yield a }
-      true
-    end
-
-    def filter
-      result = []
-      each { |a| result << a if yield a }
-      result
-    end
+  def any?
+    each { |a| return true if yield a }
+    false
   end
+
+  def all?
+    each { |a| return false unless yield a }
+    true
+  end
+
+  def filter
+    result = []
+    each { |a| result << a if yield a }
+    result
+  end
+end
