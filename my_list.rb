c33397b5bfe642d4492cbedccd9c01e4a1d8b94a
Matyas Gombos <gombosmm@gmail.com>
2022-12-22 14:06:53 -0500

    Fix initialization to run, and linters

--- my_list.rb
+++ my_list.rb
@@ -1,7 +1,7 @@
 require_relative('my_enumerable')
 class MyList
   include MyEnumerable
-  def initialize(*list)
+  def initialize(list)
     @list = list
   end
 
@@ -9,3 +9,10 @@ class MyList
     @list.each { |a| block.call a }
   end
 end
+
+list = MyList.new([1, 2, 3, 4])
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))
